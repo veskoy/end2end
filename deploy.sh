e2dcb62d52aa225be290e6a77a197f4b39196372
Veselin Stoyanov <veselin1996@gmail.com>
2016-09-24 00:05:38 +0300

    Initial customizations.

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ bundle exec jekyll build
 cd ${HTML_FOLDER}
 
 # config
-git config --global user.email "nandomoreira.me@gmail.com"
-git config --global user.name "Fernando Moreira"
+git config --global user.email "stoyanov.veseline@gmail.com"
+git config --global user.name "Veselin Stoyanov"
 
 # deploy
 git init
